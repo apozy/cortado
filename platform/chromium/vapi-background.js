58a789e3498faeaf9600d539ea8785a6cfd15dfc
Erhan Justice <bountyx@gmail.com>
2017-03-01 05:43:00 -0800

    Version bump

--- platform/chromium/vapi-background.js
+++ platform/chromium/vapi-background.js
@@ -122,7 +122,7 @@ vAPI.notifications.notifyLocked = function (domain, options) {
 
   var unlockIcon = 'data:image/svg+xml;utf8,<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M0 0h24v24H0z" fill="none"/> <path d="M12 17c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm6-9h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6h1.9c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm0 12H6V10h12v10z"/> </svg>';
   var opt = {
-              type: options.type || "basic",
+              type: options.type || "progress",
               // progress: 3,
               title: options.title || "Site Locked by Apozy",
               // contextMessage: "Risk Level"
